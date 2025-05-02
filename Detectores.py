c770167468f803b3341aa4f278f66f279d6045cc
Eider Torres <eider.torres@ib.edu.ar>
2025-05-02 10:30:13 -0500

    Instrucciones

---
+++ Detectores.py
@@ -0,0 +1,10 @@
+"""  El pulso puede generarse mediante la función puede modelarse con la función
+
+    $S(t) = A e^{-(t-t_0)^2/2 \sigma^2} \, \cos\left[\omega (t-t_0 + \sigma) \right]$
+
+    donde A se elige para que el área sea 1.
+""" 
+import numpy as np
+x=1
+y=2
+print(x+y)
