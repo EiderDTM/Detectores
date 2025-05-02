1ddd033fbb9643b47d46dcc2b69ec62e1f23b063
Eider Torres <eider.torres@ib.edu.ar>
2025-05-02 10:36:10 -0500

    Def Pulso

--- Detectores.py
+++ Detectores.py
@@ -5,6 +5,11 @@
     donde A se elige para que el área sea 1.
 """ 
 import numpy as np
-x=1
-y=2
-print(x+y)
+
+def pulso(t, t0, sigma, omega):
+    gauss = np.exp(-(t - t0)**2 / (2 * sigma**2))
+    seno = np.cos(omega * (t - t0 + sigma))
+    S = gauss * seno
+    # Normalizamos para que el área sea 1
+    A = 1 / np.trapz(S, t)
+    return A * S
